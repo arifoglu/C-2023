c524f64e3521262a7d7381f8f8950643da272b1e
arifoglu <88225121+arifoglu@users.noreply.github.com>
2024-01-17 15:20:00 +0100

    c statements outputs has been stuied

--- w3school/statements-outputs-comments/first.c
+++ w3school/statements-outputs-comments/first.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!!!");
+  //// use \n  => pass to new line 
+  printf("Hello World!!!\n");
+  printf("Salut!! \n I m learning C \n and then will be C++ \n");
+
+  // two \n will create a blank line
+  printf("Bonjour \n\n");
+  printf("Hallo \n");
+
+  // \t  create horizontal tab
+  printf("hi \t");
+  printf("hi \n");
+
+  // \\ create \ character
+  printf("hi\\hi");
+  printf("\n");
+
+  // \"  create ""
+  printf("hi I am a \"student\" ");
+
   return 0;
+
 }
